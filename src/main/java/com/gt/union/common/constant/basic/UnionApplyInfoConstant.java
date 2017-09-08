663ad3d5e0d41b231c13549884fea6dd13636648
hongjiye <461375788@qq.com>
2017-09-08 18:00:44 +0800

    重新整理

--- src/main/java/com/gt/union/common/constant/basic/UnionApplyInfoConstant.java
+++
@@ -1,7 +0,0 @@
-package com.gt.union.common.constant.basic;
-
-/**
- * Created by Administrator on 2017/8/2 0002.
- */
-public interface UnionApplyInfoConstant {
-}
--- src/main/java/com/gt/union/common/exception/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.gt.union.common.exception;
-
-public class BusinessException extends BaseException {
-    private static final long serialVersionUID = 1L;
-
-    public BusinessException(String errorLocation, String errorCausedBy, String errorMsg) {
-        super(errorLocation, errorCausedBy, errorMsg);
-    }
-}
