f7f25f2603be6e10359c5fd1d9db4237daf35a77
jerry <461375788@qq.com>
2017-09-28 10:38:03 +0800

    消费核销

--- src/main/java/com/gt/union/UnionApplication.java
+++ src/main/java/com/gt/union/UnionApplication.java
@@ -1,11 +1,16 @@
 package com.gt.union;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 /**
  * Created by Administrator on 2017/7/19 0019.
@@ -17,6 +22,20 @@ public class UnionApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
         return springApplicationBuilder.sources(UnionApplication.class);
+    }
+
+
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        /*FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        */
+        mappingJackson2HttpMessageConverter.setPrettyPrint(true);
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(mappingJackson2HttpMessageConverter);
     }
 
     public static void main(String[] args) {
--- src/main/java/com/gt/union/api/client/shop/ShopService.java
+++ src/main/java/com/gt/union/api/client/shop/ShopService.java
@@ -3,6 +3,7 @@ package com.gt.union.api.client.shop;
 import com.gt.util.entity.result.shop.WsWxShopInfoExtend;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2017/9/18 0018.
@@ -11,10 +12,10 @@ public interface ShopService {
 
 	/**
 	 * 根据商家id获取门店列表信息
-	 * @param busId		商家id
+	 * @param busId        商家id
 	 * @return
 	 */
-	List<WsWxShopInfoExtend> listByBusId(Integer busId);
+	List<Map<String, Object>> listByBusId(Integer busId);
 
 	/**
 	 * 根据多个门店id查询门店信息
