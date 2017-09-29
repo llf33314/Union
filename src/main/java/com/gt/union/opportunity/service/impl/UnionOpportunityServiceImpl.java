8996cf9742a4976aa789059755853314a5bdfe04
jerry <461375788@qq.com>
2017-09-29 09:12:39 +0800

    支付佣金回调地址两层编码

--- src/main/java/com/gt/union/opportunity/service/impl/UnionOpportunityServiceImpl.java
+++ src/main/java/com/gt/union/opportunity/service/impl/UnionOpportunityServiceImpl.java
@@ -1297,6 +1297,7 @@ public class UnionOpportunityServiceImpl extends ServiceImpl<UnionOpportunityMap
         data.put("model", ConfigConstant.PAY_MODEL);
         String encrypt = EncryptUtil.encrypt(ConfigConstant.UNION_ENCRYPTKEY, ids);
         encrypt = URLEncoder.encode(encrypt, "UTF-8");
+        encrypt = URLEncoder.encode(encrypt, "UTF-8");//两层编码
         data.put("notifyUrl", ConfigConstant.UNION_ROOT_URL + "/unionOpportunity/79B4DE7C/paymentSuccess/" + encrypt + "/" + only);
         data.put("orderNum", orderNo);//订单号
         data.put("payBusId", busId);//支付的商家id
