a8aaf695bba0df8cee4b917d17c0321397fdf7a8
HMSJ32\linweicong <2092598706@qq.com>
2017-10-12 16:50:31 +0800

    临时代码提交

--- src/main/java/com/gt/union/main/controller/UnionMainDictController.java
+++ src/main/java/com/gt/union/main/controller/UnionMainDictController.java
@@ -1,7 +1,16 @@
 package com.gt.union.main.controller;
 
+import com.gt.union.common.response.GTJsonResult;
+import com.gt.union.main.entity.UnionMainDict;
+import com.gt.union.main.service.IUnionMainDictService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,5 +23,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/unionMainDict")
 public class UnionMainDictController {
-	
+    @Autowired
+    private IUnionMainDictService unionMainDictService;
+
+    @ApiOperation(value = "根据联盟id，获取入盟申请必填信息", produces = "application/json;charset=UTF-8")
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public String listByUnionId(@PathVariable("unionId") Integer unionId) throws Exception {
+        List<UnionMainDict> result = this.unionMainDictService.listByUnionId(unionId);
+        return GTJsonResult.instanceSuccessMsg(result).toString();
+    }
 }
