8e62a2ffe35d230dcdcd6a6ccd2a4f1a5356ebb9
jerry <461375788@qq.com>
2017-10-14 11:48:28 +0800

    联盟卡

--- src/main/java/com/gt/union/card/controller/UnionCardH5Controller.java
+++ src/main/java/com/gt/union/card/controller/UnionCardH5Controller.java
@@ -62,7 +62,7 @@ public class UnionCardH5Controller extends MemberAuthorizeOrLoginController{
 						,@ApiParam(name = "url", value = "回调的url" ,required = true) @RequestParam(value = "url", required = true) String url) throws Exception{
 		Member member = SessionUtils.getLoginMember(request);
 //		member = memberService.getById(997);
-		url = "?busId=" + busId;
+		url = url + "?busId=" + busId;
 		String returnLoginUrl = this.getCardH5LoginReturnUrl(member,request,busId,url);
 		if(StringUtil.isNotEmpty(returnLoginUrl)){
 			return returnLoginUrl;
@@ -141,7 +141,7 @@ public class UnionCardH5Controller extends MemberAuthorizeOrLoginController{
 			,@ApiParam(name = "code", value = "验证码" ,required = true) @RequestParam(value = "code", required = true) String code) throws Exception{
 		Member member = SessionUtils.getLoginMember(request);
 //		member = memberService.getById(997);
-		url = "?busId=" + busId;
+		url = url + "?busId=" + busId;
 		String returnLoginUrl = this.getCardH5LoginReturnUrl(member,request,busId,url);
 		if(StringUtil.isNotEmpty(returnLoginUrl)){
 			return returnLoginUrl;
@@ -176,7 +176,7 @@ public class UnionCardH5Controller extends MemberAuthorizeOrLoginController{
 			Member member = SessionUtils.getLoginMember(request);
 //			member = memberService.getById(997);
 			Integer busId = vo.getBusId();
-			url = "?busId=" + busId;
+			url = url + "?busId=" + busId;
 			String returnLoginUrl = this.getCardH5LoginReturnUrl(member,request,busId,url);
 			if(StringUtil.isNotEmpty(returnLoginUrl)){
 				return returnLoginUrl;
