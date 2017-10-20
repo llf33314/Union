ebf4e9b00d864d1663dddc32405a034a061c8d6d
hongjiye <461375788@qq.com>
2017-10-20 10:16:11 +0800

    项目添加前端  修改项目结构

--- src/main/java/com/gt/union/api/client/socket/SocketService.java
+++
@@ -1,9 +0,0 @@
-package com.gt.union.api.client.socket;
-
-/**
- * Created by Administrator on 2017/9/21 0021.
- */
-public interface SocketService {
-
-	public int socketSendMessage(String pushName,String message,String pushStyle);
-}
--- src/main/java/com/gt/union/common/exception/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.gt.union.common.exception;
-
-public class BusinessException extends BaseException {
-    private static final long serialVersionUID = 1L;
-
-    public BusinessException(String errorMsg) {
-        super(errorMsg);
-    }
-}
