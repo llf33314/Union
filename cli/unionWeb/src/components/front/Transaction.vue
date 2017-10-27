d574eead2ea08b7e78208a798d93a2567f93fd48
jerry <461375788@qq.com>
2017-10-27 16:34:52 +0800

    办理联盟卡

--- cli/unionWeb/src/components/front/Transaction.vue
+++ cli/unionWeb/src/components/front/Transaction.vue
@@ -306,6 +306,7 @@ export default {
                 }
                 if (this.socket1) {
                   this.socket1.on('chatevent', function(data) {
+                      console.log(data);
                     if (this.visible5) {
                       let msg = eval('(' + data.message + ')');
                       _this.wxData = msg;
