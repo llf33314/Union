cae35c116233c4ea0b3bafb0d332488a252dedc5
linshukai <397789006@qq.com>
2017-12-21 16:10:47 +0800

    佣金平台接口更改

--- cli/unionBrokeragePhone/config/index.js
+++ cli/unionBrokeragePhone/config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../../../gtUnion/src/main/webapp/h5Brokerage/index.html'),
-    assetsRoot: path.resolve(__dirname, '../../../gtUnion/src/main/webapp/h5Brokerage/'),
+    index: path.resolve(__dirname, '../../../gtUnion/src/main/webapp/brokeragePhone/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../../gtUnion/src/main/webapp/brokeragePhone/'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
     productionSourceMap: true,
