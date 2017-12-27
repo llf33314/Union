01aa8402af8484ce9c4c84f617083938afc0b005
HMSJ32\linweicong <2092598706@qq.com>
2017-12-27 14:23:07 +0800

    财务模块缓存框架添加支持历史记录功能

---
+++ gtUnion/src/main/java/com/gt/union/finance/verifier/dao/impl/UnionVerifierDaoImpl.java
@@ -0,0 +1,18 @@
+package com.gt.union.finance.verifier.dao.impl;
+
+import com.gt.union.finance.verifier.entity.UnionVerifier;
+import com.gt.union.finance.verifier.mapper.UnionVerifierMapper;
+import com.gt.union.finance.verifier.dao.IUnionVerifierDao;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * 平台管理者 dao实现类
+ *
+ * @author linweicong
+ * @version 2017-12-27 10:56:25
+ */
+@Service
+public class UnionVerifierDaoImpl extends ServiceImpl<UnionVerifierMapper, UnionVerifier> implements IUnionVerifierDao {
+
+}
