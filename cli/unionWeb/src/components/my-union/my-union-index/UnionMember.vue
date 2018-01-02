a42a6f58916b06fffe7de2a414a8fe312f54555a
zhajinglan <258198274@qq.com>
2018-01-02 15:22:18 +0800

    bug修复

--- cli/unionWeb/src/components/my-union/my-union-index/UnionMember.vue
+++ cli/unionWeb/src/components/my-union/my-union-index/UnionMember.vue
@@ -29,7 +29,7 @@
         <template slot-scope="scope">
           <div class="sizeAndColor">
             <el-button size="small" @click="detail(scope)">详情</el-button>
-            <el-button size="small" v-if="isUnionOwner && !scope.row.memberOut.type" @click="remove(scope)">移出</el-button>
+            <el-button size="small" v-if="isUnionOwner && !scope.row.memberOut" @click="remove(scope)">移出</el-button>
             <el-button size="small" v-if="isUnionOwner && scope.row.memberOut.type === 2" @click="withdraw(scope)">撤回移出</el-button>
           </div>
         </template>
