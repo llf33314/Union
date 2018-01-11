3e08d7edf46c38a55602f48947daac6ae8e46687
HMSJ32\linweicong <2092598706@qq.com>
2018-01-11 11:40:16 +0800

    联盟卡小程序部分小修复

--- gtUnion/src/main/java/com/gt/union/opportunity/brokerage/service/IUnionBrokerageIncomeService.java
+++ gtUnion/src/main/java/com/gt/union/opportunity/brokerage/service/IUnionBrokerageIncomeService.java
@@ -43,15 +43,6 @@ public interface IUnionBrokerageIncomeService {
     //********************************************* Base On Business - other *******************************************
 
     /**
-     * 统计商家的佣金总和
-     *
-     * @param busId 商家id
-     * @return Double
-     * @throws Exception 统一处理异常
-     */
-    Double sumMoneyByBusId(Integer busId) throws Exception;
-
-    /**
      * 统计未删除的佣金收入总和
      *
      * @param busId 商家id
