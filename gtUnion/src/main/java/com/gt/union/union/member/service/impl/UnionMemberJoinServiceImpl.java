f50e4142d6d060e30ab6b22b6773694114a6c7eb
jerry <461375788@qq.com>
2018-01-19 15:48:33 +0800

    修复bug

--- gtUnion/src/main/java/com/gt/union/union/member/service/impl/UnionMemberJoinServiceImpl.java
+++ gtUnion/src/main/java/com/gt/union/union/member/service/impl/UnionMemberJoinServiceImpl.java
@@ -181,6 +181,13 @@ public class UnionMemberJoinServiceImpl implements IUnionMemberJoinService {
             if(busUser.getEndTime().getTime() < System.currentTimeMillis()){
                 throw new BusinessException("被推荐的商家已过期");
             }
+            List<Map> menus = busUserService.haveMenus(busUser.getId(), "s1400");
+            if(ListUtil.isEmpty(menus)){
+                throw new BusinessException("被推荐的商家没有联盟权限");
+            }
+            if(!Boolean.valueOf(menus.get(0).get("have").toString())){
+                throw new BusinessException("被推荐的商家没有联盟权限");
+            }
         } else {
             if (UnionConstant.JOIN_TYPE_APPLY_RECOMMEND != union.getJoinType()) {
                 throw new BusinessException("联盟只允许推荐入盟");
